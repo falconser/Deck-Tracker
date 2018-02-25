72c46763e8cde9f04333df8eccd8ca82ad53afe7
Sergey Ischuk <falcon.ser@gmail.com>
2018-02-26 01:24:18 +0200

    Dev cleanup of DeckTrackerWatch

--- Deck Tracker WatchKit Extension/DeckTrackerWatch.swift
+++ Deck Tracker WatchKit Extension/DeckTrackerWatch.swift
@@ -23,11 +23,11 @@ class DeckTrackerWatch: WKInterfaceController {
     let groupDefaults = UserDefaults(suiteName: "group.com.falcon.Deck-Tracker.Decks")
     let defaults = UserDefaults.standard
     
-    var coinSwitchInt:Int = 0
-    var winSwitchInt:Int = 1
-    var selectedDeckName: String = ""
-    var selectedDeckClass: String = ""
-    var selectedTag:String = ""
+    var coin = false
+    var win = true
+    var selectedDeckName = ""
+    var selectedDeckClass = ""
+    var selectedTag = ""
 
 
     override func awake(withContext context: Any?) {
@@ -45,66 +45,40 @@ class DeckTrackerWatch: WKInterfaceController {
         setTagButton()
     }
 
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
-    
     @objc @IBAction func saveButtonPressed() {
         
         // Gathers the data to make the dict to send the info to the phone
-        let dict = NSMutableDictionary()
+        var dict = [String:Any]()
         // Gets selected deck and selected class associated with the deck
         if let deckName = groupDefaults?.string(forKey:"Selected Deck Name"),
             let deckClass = groupDefaults?.string(forKey:"Selected Deck Class")
         {
             selectedDeckName = deckName
             selectedDeckClass = deckClass
-            dict.setValue(selectedDeckName, forKey: "selectedDeckName")
-            dict.setValue(selectedDeckClass, forKey: "selectedDeckClass")
+            dict["selectedDeckName"] = selectedDeckName
+            dict["selectedDeckClass"] = selectedDeckClass
         }
         
-        // Gets the Opponent Class
-        let opponentClass = defaults.string(forKey:"Watch Opponent Class")
-        dict.setValue(opponentClass, forKey: "watchOpponentClass")
-        
-        // Gets if user had coin or not
-        var coin:Bool = false
-        if coinSwitchInt == 0 {
-            coin = false
-        } else {
-            coin = true
-        }
-        dict.setValue(coin, forKey: "coin")
-        
-        // Gets if user won or not
-        var win:Bool = true
-        if winSwitchInt == 0 {
-            win = false
-        } else {
-            win = true
-        }
-        dict.setValue(win, forKey: "win")
-        
+        dict["coin"] = coin
+        dict["win"] = win
+
         // Gets the selected tag
         selectedTag = defaults.string(forKey:"Selected Tag Watch") ?? ""
-        
-        dict.setValue(selectedTag, forKey: "watchSelectedTag")
+        dict["watchSelectedTag"] = selectedTag
         
         // Saves the dictionary and sends the info to the phone
-        if opponentClass != nil {
-                WKInterfaceController.openParentApplication(["Save New Game" : ""] , reply: { [](reply, error) -> Void in
-                    })
+        // Gets the Opponent Class
+        if let opponentClass = defaults.string(forKey:"Watch Opponent Class") {
+            dict["watchOpponentClass"] = opponentClass
             if let _ = groupDefaults?.string(forKey:"Selected Deck Name") {
                 groupDefaults?.set(dict, forKey: "Add Game Watch")
                 groupDefaults?.synchronize()
+                WCSession.default.transferUserInfo(["Save New Game" : ""])
             } else {
                 saveGameButton.setTitle("Select a deck!")
             }
-
-            
-        } else {
+        }
+        else {
             saveGameButton.setTitle("Opponent Class needed!")
         }
         
@@ -131,7 +105,6 @@ class DeckTrackerWatch: WKInterfaceController {
     func setOpponentClassButton() {
         // Populates the opponent class button
         if let opponentClass = defaults.string(forKey:"Watch Opponent Class") {
-            //selectOpponentButton.setTitle("Opponent: " + String(opponentClass))
             colorCell(classToBeColored: opponentClass, button: selectOpponentButton, opponent: true, deckName: "")
             saveGameButton.setTitle("Save Game")
         } else {
@@ -151,134 +124,42 @@ class DeckTrackerWatch: WKInterfaceController {
         // Colors the cell according to the Class specified
         if classToBeColored == "Warrior" {
             button.setBackgroundColor(UIColor(rgbValue:0xCC0000))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-               let attibuteString = NSAttributedString(string: "Versus: Warrior", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Paladin" {
             button.setBackgroundColor(UIColor(rgbValue:0xCCC333))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Paladin", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Shaman" {
             button.setBackgroundColor(UIColor(rgbValue:0x3366CC))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Shaman", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Hunter" {
             button.setBackgroundColor(UIColor(rgbValue:0x339933))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Hunter", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Druid" {
             button.setBackgroundColor(UIColor(rgbValue:0x990000))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Druid", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Rogue" {
             button.setBackgroundColor(UIColor(rgbValue:0x666666))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Rogue", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Warlock" {
             button.setBackgroundColor(UIColor(rgbValue:0x9900CC))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Warlock", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Mage" {
             button.setBackgroundColor(UIColor(rgbValue:0x009999))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Mage", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         } else if classToBeColored == "Priest" {
             button.setBackgroundColor(UIColor(rgbValue:0x999999))
-            let boldFont = UIFont(name: "Helvetica Neue", size: 15.0)!
-            let greenColor = UIColor.green
-            let attributeDictionary = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: greenColor]
-            if opponent == true {
-                let attibuteString = NSAttributedString(string: "Versus: Priest", attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            } else {
-                let attibuteString = NSAttributedString(string: "Deck: " + deckName, attributes: attributeDictionary)
-                button.setAttributedTitle(attibuteString)
-            }
         }
+
+        let text = opponent ? "Versus: \(classToBeColored)" : "Deck: \(deckName)"
+        let attributedTitle = NSAttributedString(string: text,
+                                                 attributes: [NSAttributedStringKey.font: UIFont(name: "Helvetica Neue", size: 15.0)!,
+                                                              NSAttributedStringKey.foregroundColor: UIColor.green])
+        button.setAttributedTitle(attributedTitle)
     }
     
     
     
     @objc @IBAction func coinSwitchToggled(value: Bool) {
         // Keep track of coin switch
-        if value {
-            coinSwitchInt = 1
-        } else {
-            coinSwitchInt = 0
-        }
+        coin = value
     }
     
     
     @objc @IBAction func winSwitchToggled(value: Bool) {
         // Keep track of win switch
-        if value {
-            winSwitchInt = 1
-        } else {
-            winSwitchInt = 0
-        }
+        win = value
     }
     
     func setTagButton() {
@@ -289,9 +170,6 @@ class DeckTrackerWatch: WKInterfaceController {
         } else {
             tagsButton.setTitle("Add Tag")
         }
-        //println(selectedTag)
     }
-    
-    
 
 }
